a3137b2de4cfb9687ae6dd3c4be35625bce9a22f
Harshit Maurya <harsitmaurya555@gmail.com>
2026-09-18 23:53:38 +0530

    Solve covariance-matrix

--- covariance-matrix/covariance-matrix.py
+++ covariance-matrix/covariance-matrix.py
@@ -5,8 +5,7 @@ def covariance_matrix(X: list) -> np.ndarray:
     Returns the covariance matrix as a NumPy array.
     """
     # Write code here
-    x = np.asarray(X,dtype=float)
-    y = x - np.mean(x,axis=0)
-    z = (y.T @ y)/(x.shape[0]-1)
-    return z
-    pass
+    X = np.asarray(X,dtype='float')
+    X = X-np.mean(X,axis=0)
+    N = X.shape[0]
+    return (X.T @ X) / (N-1)
