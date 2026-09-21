760c63f50de97b5861ce8429f7310205c034c90f
Harshit Maurya <harsitmaurya555@gmail.com>
2026-09-21 23:55:27 +0530

    Solve sample-var-std

---
+++ sample-var-std/sample-var-std.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def sample_var_std(x: list) -> dict:
+    """
+    Returns a dictionary with variance and standard_deviation.
+    """
+    # Write code here
+    x = np.asarray(x,dtype=float)
+    mean = np.mean(x,dtype=float)
+    var = 1/(len(x)-1)*np.sum((x-mean)**2)
+    std = np.sqrt(var)
+    return {"variance": float(var), "standard_deviation": float(std)}
